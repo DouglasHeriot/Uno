0d55207dbc9ba30c0e1660faa21b2df4f0a42572
Douglas Heriot <dhd@douglasheriot.com>
2010-04-05 16:23:29 +1000

    Added GameOptions class

---
+++ Uno/GameOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Uno
+{
+    class GameOptions
+    {
+
+        // TODO: break down into properties with private attributes
+
+
+        public int CardsForEachPlayer = 7;
+
+    }
+}
